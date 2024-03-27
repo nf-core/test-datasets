b92cca5af71822688071b83967e32ac099ea98a1
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2024-03-26 21:28:19 -0700

    get list of omics keys

--- omics-2024-02-19/ omics-cmd.sh
+++ omics-2024-02-19/ omics-cmd.sh
@@ -38,12 +38,34 @@ arn:aws:omics:us-east-1:850787717197:sequenceStore/1867753048
 AWS_PROFILE=sales aws omics get-sequence-store --id 1867753048
 AWS_PROFILE=sales aws omics list-read-sets --sequence-store-id 1867753048
 AWS_PROFILE=sales aws omics get-read-set-metadata --id 5091960702 --sequence-store-id 1867753048 | jq -r .files.source1.s3Access.s3Uri
-AWS_PROFILE=sales aws s3 ls s3://850787717197-1867753-fepwgrx9iujr5b9pkjudkhpgxwbuhuse1b-s3alias/850787717197/sequenceStore/1867753048/readSet/5091960702
+AWS_PROFILE=sales aws s3 ls s3://850787717197-1867753-fepwgrx9iujr5b9pkjudkhpgxwbuhuse1b-s3alias/850787717197/sequenceStore/1867753048/readSet/5091960702/
+
+AWS_PROFILE=sales aws s3 ls s3://850787717197-1867753-fepwgrx9iujr5b9pkjudkhpgxwbuhuse1b-s3alias/850787717197/sequenceStore/1867753048/readSet/ --recursive
 
 AWS_PROFILE=sales aws s3 ls s3://850787717197-1867753-fepwgrx9iujr5b9pkjudkhpgxwbuhuse1b-s3alias/850787717197/sequenceStore/1867753048/readSet/5091960702/HG00405.final.cram
 
 ## S3API
 
-AWS_PROFILE=sales aws s3api get-object --bucket 850787717197-1867753-fepwgrx9iujr5b9pkjudkhpgxwbuhuse1b-s3alias --key 850787717197/sequenceStore/1867753048/readSet/5091960702/HG00405.final.cram HG00405.final.cram
+AWS_PROFILE=sales aws s3api get-object --bucket 850787717197-1867753-fepwgrx9iujr5b9pkjudkhpgxwbuhuse1b-s3alias --key 850787717197/sequenceStore/1867753048/readSet/5091960702/HG00405.final.cram.crai HG00405.final.cram.crai
 
-AWS_PROFILE=sales aws s3api list-object-versions --bucket 850787717197-1867753-fepwgrx9iujr5b9pkjudkhpgxwbuhuse1b-s3alias --key 850787717197/sequenceStore/1867753048/readSet/5091960702/HG00405.final.cram
+```json
+{
+    "AcceptRanges": "bytes",
+    "LastModified": "2024-03-12T23:46:42+00:00",
+    "ContentLength": 1334307,
+    "ETag": "\"5c981ab5c452b538fafabb6574e66c82-1\"",
+    "VersionId": "kDds52DrVLhFe24U38o6BIxIo8oL9kgS",
+    "ContentType": "binary/octet-stream",
+    "ServerSideEncryption": "aws:kms",
+    "Metadata": {},
+    "SSEKMSKeyId": "arn:aws:kms:us-east-1:559620149354:key/c46b5818-b660-4e2b-ad4a-721b24d60d3b",
+    "BucketKeyEnabled": true,
+    "TagCount": 4
+}
+```
+# list-objects-v2
+# list-object-versions
+
+AWS_PROFILE=sales aws s3api list-objects-v2 --bucket 850787717197-1867753-fepwgrx9iujr5b9pkjudkhpgxwbuhuse1b-s3alias 
+
+AWS_PROFILE=sales aws s3api list-object-versions --bucket 850787717197-1867753-fepwgrx9iujr5b9pkjudkhpgxwbuhuse1b-s3alias --key 850787717197/sequenceStore/1867753048/readSet/5091960702/HG00405.final.cram.crai
