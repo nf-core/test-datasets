acf689ccb1ec530025138e50fa075908de2ac5f4
LouisBzh <louislenezet@gmail.com>
2024-07-15 16:07:00 +0200

    Update individuals data

--- get_ind_snp.sh
+++ get_ind_snp.sh
@@ -11,47 +11,52 @@
 
 DIR_IND=$1
 DIR_PANEL=$2
-REF_FASTA=$3
-SNP_FILE=$4
-IND_SEL=$5
-REGION_LST=$6
+PANEL_NAME=$3
+REF_FASTA=$4
+SNP_FILE=$5
+IND_SEL=$6
+REGION_LST=$7
 
-# Set region file to bcftools format
-REGION_CSV=${REGION_LST}.csv
-awk -v OFS='\t' -F':' '{split($2, coords, "-"); print $1, coords[1], coords[2]}' ${REGION_LST} > ${REGION_CSV}
+# Create full tsv file from legend file
+> ${DIR_PANEL}/${PANEL_NAME}.tsv
+for REGION in $( cat $REGION_LST );
+do
+    CHR=$(echo $REGION | cut -d':' -f1)
+    zcat ${DIR_PANEL}/$CHR/${PANEL_NAME}.${CHR}.legend.gz | \
+        awk -v OFS='\t' 'NR>1 { split($1, a, "[:-_]"); print a[1], $2, $3 "," $4 }' >> ${DIR_PANEL}/${PANEL_NAME}.tsv
+done
+
+bgzip ${DIR_PANEL}/${PANEL_NAME}.tsv
+tabix -s1 -b2 -e2 ${DIR_PANEL}/${PANEL_NAME}.tsv.gz
+
+TSV=${DIR_PANEL}/${PANEL_NAME}.tsv.gz
 
 while IFS="," read IND; do
     # Get the genotype likelihood based on the panel for the validation file
     echo 'Get the genotype likelihood based on the panel for the validation file'
-    VCF=${PANEL_NAME}.sites.vcf.gz
-    TSV=${PANEL_NAME}.tsv.gz
+
+    IND_DIR=${DIR_IND}/${IND}
+    IND_FILE=${IND_DIR}/${IND}.s
 
     bcftools mpileup -f ${REF_FASTA} \
-        -I -E -a 'FORMAT/DP' -T ${VCF} \
+        -I -E -a 'FORMAT/DP' -T ${TSV} \
         ${IND_S}.bam -Ou | \
         bcftools call -Aim -C alleles -T ${TSV} | \
         bcftools annotate --set-id '%CHROM\:%POS\:%REF\:%FIRST_ALT' -Ob -o ${IND_S}.bcf
     bcftools index -f ${IND_S}.bcf
-
-    # Compute genotype likelihood based on the panel
-    echo 'Compute genotype likelihood based on the panel'
-    bcftools mpileup -f ${REF_FASTA} \
-        -I -E -a 'FORMAT/DP' -T ${VCF} \
-        --regions-file ${REGION_CSV} ${IND_S}.1x.bam -Ou |
-        bcftools call -Aim -C alleles -T ${TSV} | \
-        bcftools annotate --set-id '%CHROM\:%POS\:%REF\:%FIRST_ALT' -Ob -o ${IND_S}.1x.bcf
-    bcftools index -f ${IND_S}.1x.bcf
 
     # Get individual SNP
     echo 'Get individual SNP'
     plink --bcf ${IND_S}.bcf \
         --allow-extra-chr \
         --geno 0 \
-        --make-bed --out ${IND_S}
+        --make-bed --out ${IND_S}/tmp/${IND}
+
     # Replace all variants id by .
-    awk '{$2 = "."; print}' ${IND_S}.bim > ${IND_S}_NID.bim
-    plink --bfile ${IND_S} \
-        --bim ${IND_S}_NID.bim \
+    mkdir -p ${IND_DIR}/tmp
+    awk '{$2 = "."; print}' ${IND_DIR}/tmp/${IND} > ${IND_DIR}/tmp/${IND}_NID.bim
+    plink --bfile ${IND_DIR}/tmp/${IND} \
+        --bim ${IND_DIR}/tmp/${IND}_NID.bim \
         --allow-extra-chr \
         --set-missing-var-ids chr@:# \
         --extract ${SNP_FILE} \
