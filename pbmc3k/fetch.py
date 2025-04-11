ada2b88ebc28e8ce39abd1d857a02fbdf5e83138
nictru <nictru32@gmail.com>
2025-04-11 19:20:03 +0200

    Set random seed for PBMC3k CSV subsampling

--- pbmc3k/fetch.py
+++ pbmc3k/fetch.py
@@ -14,7 +14,7 @@ adata_raw.write_h5ad('pbmc3k_raw_geneid.h5ad')
 adata_processed = sc.datasets.pbmc3k_processed()
 adata_processed.var.index.name = None
 adata_processed.write_h5ad('pbmc3k_processed.h5ad')
-sc.pp.sample(adata_processed, n=500, copy=True).to_df().to_csv('pbmc3k_processed.csv')
+sc.pp.sample(adata_processed, n=500, copy=True, rng=0).to_df().to_csv('pbmc3k_processed.csv')
 
 geneid_mapping = sc.datasets.pbmc3k().var["gene_ids"].to_dict()
 adata_processed.var.index = adata_processed.var.index.map(geneid_mapping)
