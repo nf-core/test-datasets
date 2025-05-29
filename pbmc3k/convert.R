252c2f198af9bbb80cad22ef5786ffe18d1535d4
nictru <nictru32@gmail.com>
2025-05-29 17:51:33 +0200

    Switch to PBMC test data

--- pbmc3k/convert.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-library(Seurat)
-
-sce <- readRDS("pbmc3k_processed_sce.rds")
-
-seurat_obj <- as.Seurat(sce, data = NULL)
-
-saveRDS(seurat_obj, file = "pbmc3k_processed_seurat.rds")
